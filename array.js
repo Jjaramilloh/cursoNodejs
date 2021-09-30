48dc7719968ae4e9adcf2a41a630f13aefaefb26
Jjaramilloh <72838507+Jjaramilloh@users.noreply.github.com>
2021-09-30 15:11:30 -0500

    first commit

---
+++ array.js
@@ -0,0 +1,11 @@
+/**
+ * autor Jorge Jaramillo
+ * escrito el dia 29 de septiembre de 2021
+ */
+
+/**
+ * funcion que contiene arrays de frutas
+ */
+const getfrutas=()=>{
+    return ["Piña","Fresa","Manzana","Uva"]
+}
